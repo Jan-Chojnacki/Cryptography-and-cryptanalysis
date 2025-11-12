2bb1ae511f2659e6ee544e1db8061353a5b99acc
Jan Chojnacki <jan@chojnacki.dev>
2025-11-12 21:47:13 +0100

    Improves performance of ngram generator
    Optimizes the ngram generator by using unsafe code
    to directly access and modify the ngram array. This
    avoids bounds checking for improved performance.

--- src/analysis/fast_ngram_generator.rs
+++ src/analysis/fast_ngram_generator.rs
@@ -1,11 +1,9 @@
 pub fn fast_ngram_generator(input: &Vec<u8>) -> [f32; 676] {
     let mut ngram: [f32; 676] = [0.0; 676];
 
-    input
-        .windows(2)
-        .for_each(|w| {
-            ngram[(w[0] - b'A') as usize * 26 + (w[1] - b'A') as usize] += 1.0;
-        });
+    input.windows(2).for_each(|w| unsafe {
+        *ngram.get_unchecked_mut((w[0] - b'A') as usize * 26 + (w[1] - b'A') as usize) += 1.0;
+    });
 
     ngram
 }
